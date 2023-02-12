5d762ec1dd58510d7b3359c0259721e47d4899e6
NuclearBlock <nuclearblockone@gmail.com>
2023-02-12 17:37:19 +0300

    add settings obj

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../../context/main';
 import Box from '@mui/material/Box';
 import { NavLink } from "react-router-dom";
-
 import Search from './Search';
-
 import PersonIcon from '@mui/icons-material/Person';
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import WidgetsOutlinedIcon from '@mui/icons-material/WidgetsOutlined';
@@ -18,9 +17,9 @@ import { Stack } from '@mui/material';
 
 export default function Header() {
 
+  const {settings} = useContext(AppContext);
+
   const [data, setData] = useState({
-    host: "127.0.0.1",
-    port: 8899,
     currentEpoch: 0,
     currentBlock: 0,
     txCount:0,
@@ -31,80 +30,80 @@ export default function Header() {
   }
 
   return (
-    <>
-    <div id="menu">
-      <Grid container spacing={1}>
-        <Grid item xs={8} margin="auto">
-          <NavLink to="accounts/">
-            <PersonIcon fontSize="medium" />
-            Accounts
-          </NavLink>
+    <Box component="header">
+      <Box id="menu">
+        <Grid container spacing={1}>
+          <Grid item xs={8} margin="auto">
+            <NavLink to="accounts/">
+              <PersonIcon fontSize="medium" />
+              Accounts
+            </NavLink>
 
-          <NavLink to="contracts/">
-            <AccountTreeIcon fontSize="medium" />
-            Contracts
-          </NavLink>
+            <NavLink to="contracts/">
+              <AccountTreeIcon fontSize="medium" />
+              Contracts
+            </NavLink>
 
-          <NavLink to="blocks/">
-            <WidgetsOutlinedIcon fontSize="medium" />
-            Blocks
-          </NavLink>
+            <NavLink to="blocks/">
+              <WidgetsOutlinedIcon fontSize="medium" />
+              Blocks
+            </NavLink>
 
-          <NavLink to="txs/">
-            <AssignmentOutlinedIcon fontSize="medium"/>
-            Transactions
-          </NavLink>
+            <NavLink to="txs/">
+              <AssignmentOutlinedIcon fontSize="medium"/>
+              Transactions
+            </NavLink>
 
-          <NavLink to="logs/">
-            <TerminalIcon fontSize="medium"/>
-            Logs
-          </NavLink>
+            <NavLink to="logs/">
+              <TerminalIcon fontSize="medium"/>
+              Logs
+            </NavLink>
 
-          <NavLink to="faucet/">
-            <OpacityIcon fontSize="medium"/>
-            Faucet
-          </NavLink>
+            <NavLink to="faucet/">
+              <OpacityIcon fontSize="medium"/>
+              Faucet
+            </NavLink>
 
-          <NavLink to="wallet/">
-            <AccountBalanceWalletIcon fontSize="medium" />
-            Wallet
-          </NavLink>
+            <NavLink to="wallet/">
+              <AccountBalanceWalletIcon fontSize="medium" />
+              Wallet
+            </NavLink>
+          </Grid>
+
+          <Grid item xs={4} >
+            <Search />
+          </Grid>
         </Grid>
+      </Box>
 
-        <Grid item xs={4} >
-          <Search />
+      <Box id="submenu">
+        <Grid container spacing={1}>
+
+          <Grid item xs={8} margin="auto" >
+              <span>RPC Node: {settings.validatorHostame}:{settings.vaidatorPort}</span>
+              <span>Current epoch: {data.currentEpoch}</span>
+              <span>Current blocks: {data.currentBlock}</span>
+              <span>Txs total: {data.txCount}</span>
+          </Grid>
+
+          <Grid item xs={4} textAlign="center" margin={"auto"}>
+          <Stack 
+            direction="row"
+            justifyContent="center"
+            spacing={2}
+            >
+            <NavLink to="start/">
+              <ReplayIcon fontSize="medium" />
+            </NavLink>
+            <NavLink to="settings/">
+              <SettingsIcon fontSize="medium" />
+            </NavLink>
+            </Stack>
+          </Grid>
+
         </Grid>
-      </Grid>
-    </div>
-
-    <div id="submenu">
-      <Grid container spacing={1}>
-
-        <Grid item xs={8} margin="auto" >
-            <span>RPC Node: {data.host}:{data.port}</span>
-            <span>Current epoch: {data.currentEpoch}</span>
-            <span>Current blocks: {data.currentBlock}</span>
-            <span>Txs total: {data.txCount}</span>
-        </Grid>
-
-        <Grid item xs={4} textAlign="center" margin={"auto"}>
-        <Stack 
-          direction="row"
-          justifyContent="center"
-          spacing={2}
-          >
-          <NavLink to="start/">
-            <ReplayIcon fontSize="medium" />
-          </NavLink>
-          <NavLink to="settings/">
-            <SettingsIcon fontSize="medium" />
-          </NavLink>
-          </Stack>
-        </Grid>
-
-      </Grid>
-    </div>
-    </>
+      </Box>
+    </Box>
   )
 
 }
