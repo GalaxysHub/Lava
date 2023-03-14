ec37de78906b4828c71d192ce38d457ff1ee2384
NuclearBlock <nuclearblockone@gmail.com>
2023-03-15 01:17:10 +0300

    Add program test types

--- src/libs/types.ts
+++ src/libs/types.ts
@@ -1,6 +1,36 @@
 import { Keypair, PublicKey } from "@solana/web3.js";
 import { Idl } from "@project-serum/anchor";
+import { IdlInstruction, IdlAccountItem, IdlField } from "@project-serum/anchor/dist/cjs/idl";
 
+
+export type TestInstuctionArgument = {
+  idlArg: IdlField[],
+  value: string | number,
+}
+
+export type TestInstuctionAccount = {
+  idlAccount: IdlAccountItem,
+  value:  TAccount | [number, PublicKey],
+}
+
+export type TestInstuction = {
+  // idlInstruction: IdlInstruction,
+  args: TestInstuctionArgument[],
+  accounts: TestInstuctionAccount[];
+}
+
+// export type TestInstuctionArgument = string | number;
+
+// export type TestInstuctionAccount = TAccount | [number, PublicKey];
+
+// export type TestInstuction = {
+//   args: TestInstuctionArgument[],
+//   accounts: TestInstuctionAccount[];
+// }
+
+export type ProgramTest = {
+  instructions: TestInstuction[],
+}
 
 export type TAccount = {
   alias: string,
@@ -19,6 +49,7 @@ export type TProgram = {
   idl?: Idl,
   cluster: TCluster,
   pdas: Record<string, [PublicKey, number]>;
+  tests?: ProgramTest[],
 }
 
 export type TCluster = {
